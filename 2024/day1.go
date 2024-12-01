f84fe29928affbd093d6ecc97c764f95481d098c
Tim <tpham2580@gmail.com>
2024-12-01 15:10:45 -0800

    finished day 1

--- 2024/day1.go
+++ 2024/day1.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	// TODO: Read line by line through day1input.txt, place first value into first list and second value into second list
+	// Read line by line through day1input.txt, place first value into first list and second value into second list
 	file, err := os.Open("day1input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -21,6 +21,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	var arr1 = []int{}
 	var arr2 = []int{}
+	counts2 := make(map[int]int)
 
 	var lineNumber int = 0
 	for scanner.Scan() {
@@ -41,25 +42,34 @@ func main() {
 		if err != nil {
 			log.Fatal("Failed to convert string to int")
 		}
+		counts2[num2]++ // increment the second integer count
 
 		arr1 = append(arr1, num1)
 		arr2 = append(arr2, num2)
 	}
 
-	// TODO: Sort the two lists
+	// Sort the two lists
 	sort.Ints(arr1)
 	sort.Ints(arr2)
 
-	// TODO: Go through both list at the same time, if the values are the same, add to running total
+	// Go through both list at the same time, if the values are the same, add to running total
 	var total int = 0
+	var similarity int = 0
 	for i := 0; i < len(arr1); i++ {
+		// Increase total distance
 		if arr1[i] < arr2[i] {
 			total += arr2[i] - arr1[i]
 		} else {
 			total += arr1[i] - arr2[i]
 		}
+
+		// Increase similarity by count * value
+		if count, exists := counts2[arr1[i]]; exists {
+			similarity += count * arr1[i]
+		}
 	}
 
-	// TODO: Output the running total
+	// Output the running total
 	fmt.Printf("The total distance between left and right lists is: %d\n", total)
+	fmt.Printf("The total similarity between left and right lists is: %d\n", similarity)
 }
