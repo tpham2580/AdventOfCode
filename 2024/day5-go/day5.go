ad17288cf06c4ccba70d396275dc091f586df3df
Tim <tpham2580@gmail.com>
2024-12-13 17:02:37 -0800

    added readfile boilerplate

--- 2024/day5-go/day5.go
+++ 2024/day5-go/day5.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"aoc-go-2024/internal/logger"
+	"bufio"
 	"flag"
+	"os"
 )
 
 type Service struct {
 	log *logger.Logger
+}
+
+// Reads from file and return a adjacency map and an array of array of integers to process
+func (s *Service) readFile(fileName string) (map[int][]int, [][]int) {
+	var adj = make(map[int][]int)
+	var arrInt [][]int
+	file, err := os.Open(fileName)
+	if err != nil {
+		s.log.Error(err.Error())
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+	}
+
+	return adj, arrInt
 }
 
 func main() {
@@ -17,4 +36,6 @@ func main() {
 	// Flag
 	file := flag.String("file", "day5input.txt", "Name of file that is being read")
 	flag.Parse()
+
+	adj, arrInt := s.readFile(*file)
 }
